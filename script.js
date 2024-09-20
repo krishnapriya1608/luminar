7fe5fe3a5de919c968cf3454c4f94a2714ef0bf7
krishna1608 <rishnak10@gmail.com>
2024-09-20 16:55:03 +0530

    message

---
+++ script.js
@@ -0,0 +1,74 @@
+
+const navbar=document.querySelector(`.revimage`)
+gsap.from(navbar.children,{
+    opacity:0,
+    x:20,
+    duration:3,
+    display:1.5,
+
+    stagger:{
+       amount:1
+    }
+})
+const navbar1=document.querySelector(`.revimage1`)
+gsap.from(navbar1.children,{
+    opacity:0,
+    x:20,
+    duration:3,
+    display:1.5,
+    delay:1,
+    
+    stagger:{
+       amount:1
+       
+    }
+})
+const navvbar=document.querySelector(`.revimage3`)
+gsap.from(navvbar.children,{
+    opacity:0,
+    x:20,
+    duration:3,
+    display:1.5,
+    delay:3,
+ 
+    stagger:{
+       amount:1
+    }
+})
+const navbbar1=document.querySelector(`.revimage4`)
+gsap.from(navbbar1.children,{
+    opacity:0,
+    x:20,
+    duration:3,
+    display:1.5,
+    delay:3,
+    
+    stagger:{
+       amount:1
+    }
+})
+const naavbar=document.querySelector(`.revimage5`)
+gsap.from(naavbar.children,{
+    opacity:0,
+    x:20,
+    duration:3,
+    display:1.5,
+    delay:6,
+    
+    stagger:{
+       amount:1
+       
+    }
+})
+const navbarr1=document.querySelector(`.revimage6`)
+gsap.from(navbarr1.children,{
+    opacity:0,
+    x:20,
+    duration:3,
+    display:1.5,
+    delay:5,
+    
+    stagger:{
+       amount:1
+    }
+})
